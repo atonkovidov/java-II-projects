0e9987a6ca2dab8c7e2d8b65db205b8f3a9d3296
atonkovidov <59673687+atonkovidov@users.noreply.github.com>
2020-06-29 18:25:50 -0700

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,22 @@
+interface NumericFunc {
+	int func(int n);
+}
+
+//class FactorialLambdaDemo {
+class Assignment1 {
+
+	public static void main(String args[]) {
+		// This block lambda computes the factorial of an int value.
+		
+		NumericFunc factorial = n -> {
+			int value = 1;
+			for (int i = 1; i <= n; i++)
+				value *= i;
+			return value;
+		};
+		
+		System.out.println("The factoral of 3 is " + factorial.func(3));
+		System.out.println("The factoral of 5 is " + factorial.func(5));
+		System.out.println("The factoral of 9 is " + factorial.func(9));
+	}
+}
